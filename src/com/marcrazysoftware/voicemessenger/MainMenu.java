a661ce7345de7a94d85a8b392c4b2558fb329102
Daniel Marchese <marchese.daniel@gmail.com>
2013-03-14 13:45:10 -0400

    -Added click listeners

--- src/com/marcrazysoftware/voicemessenger/MainMenu.java
+++ src/com/marcrazysoftware/voicemessenger/MainMenu.java
@@ -3,16 +3,57 @@ package com.marcrazysoftware.voicemessenger;
 import java.util.List;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
+import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
+import android.widget.Button;
+import android.widget.ListView;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 
 @SuppressWarnings("unused")
 public class MainMenu extends Activity implements OnInitListener,
 		RecognitionListener {
 
+	private static final int TTS_CODE = 12345;
+
+	private Button micButton;
+	private Button sendMessageButton;
+	private ListView messageListView;
+
+	private TextToSpeech TTS;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		/*
+		 * TODO: Set the widgets if speech recognition is available.
+		 */
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	protected void onDestroy() {
+		if (this.TTS != null) {
+			this.TTS.stop();
+			this.TTS.shutdown();
+		}
+		super.onDestroy();
+	}
+	
+	private void runSpeechRecognition() {
+		
+	}
+
 	@Override
 	public void onBeginningOfSpeech() {
-
+		this.micButton.setEnabled(false);
 	}
 
 	@Override
@@ -22,7 +63,7 @@ public class MainMenu extends Activity implements OnInitListener,
 
 	@Override
 	public void onEndOfSpeech() {
-
+		this.micButton.setEnabled(true);
 	}
 
 	@Override
@@ -47,7 +88,8 @@ public class MainMenu extends Activity implements OnInitListener,
 
 	@Override
 	public void onResults(Bundle results) {
-
+		resultDispatcher(results
+				.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION));
 	}
 
 	@Override
@@ -64,4 +106,12 @@ public class MainMenu extends Activity implements OnInitListener,
 
 	}
 
+	private boolean isConnected() {
+		PackageManager pm = this.getPackageManager();
+		return false;
+	}
+	
+	private boolean hasSpeechRecognition() {
+		return false;
+	}
 }
