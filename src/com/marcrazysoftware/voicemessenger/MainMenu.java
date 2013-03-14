bdf26732f6abc26e82b6b8716c7c1753c80508be
Daniel Marchese <marchese.daniel@gmail.com>
2013-03-14 11:36:46 -0400

    -Implemented the widgets, no more errors now.

--- src/com/marcrazysoftware/voicemessenger/MainMenu.java
+++ src/com/marcrazysoftware/voicemessenger/MainMenu.java
@@ -1,27 +1,52 @@
 package com.marcrazysoftware.voicemessenger;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 import android.app.Activity;
 
 public class MainMenu extends Activity {
-	
+
 	/*
 	 * Class member variables
 	 */
 	private Button mMicrophoneButton;
 	private ListView mMessageList;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_menu_layout);
-        setWidgets();
-    }
-    
-    private void setWidgets() {
-    	this.mMicrophoneButton = (Button) findViewById(R.id.bMicrophone);
-    }
-    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_menu_layout);
+		setWidgets();
+	}
+
+	private void setWidgets() {
+		this.mMicrophoneButton = (Button) findViewById(R.id.bMicrophone);
+		this.mMessageList = (ListView) findViewById(R.id.svMessageView);
+
+		this.mMicrophoneButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getBaseContext(), "Mic Button Clicked",
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		this.mMessageList.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	}
+
 }
