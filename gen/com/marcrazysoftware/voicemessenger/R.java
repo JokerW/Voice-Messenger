31ed8655f36a1baa356479eac17ffa788c8d3a9f
Daniel Marchese <marchese.daniel@gmail.com>
2013-03-14 16:07:22 -0400

    -Added SendMessage Activity and Layout

--- gen/com/marcrazysoftware/voicemessenger/R.java
+++ gen/com/marcrazysoftware/voicemessenger/R.java
@@ -25,11 +25,13 @@ public final class R {
     }
     public static final class id {
         public static final int LinearLayout1=0x7f070000;
+        public static final int OverallMessagingLayout=0x7f070003;
         public static final int bMicrophone=0x7f070002;
         public static final int lvMessageView=0x7f070001;
     }
     public static final class layout {
         public static final int main_menu_layout=0x7f030000;
+        public static final int send_message_layout=0x7f030001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
