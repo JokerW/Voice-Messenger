fd9a900c291634fb92b8843621692d42e3092a07
Daniel Marchese <marchese.daniel@gmail.com>
2013-03-15 14:52:19 -0400

    -Added PrefsActivity.java to the manifest

--- src/com/marcrazysoftware/voicemessenger/PrefsActivity.java
+++ src/com/marcrazysoftware/voicemessenger/PrefsActivity.java
@@ -1,7 +1,21 @@
 package com.marcrazysoftware.voicemessenger;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
-public class PrefsActivity extends Activity {
+@SuppressWarnings("deprecation")
+public class PrefsActivity extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		/*
+		 * I'm aware that this is deprecated, I still use it to keep backwards
+		 * compatibility. When more people move to ICS, this will be done the
+		 * recommended way with fragments.
+		 */
+		addPreferencesFromResource(R.xml.preferences_layout);
+	}
 
 }
