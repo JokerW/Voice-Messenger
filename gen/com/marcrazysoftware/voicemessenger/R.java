0644388ad1de85bd9a6ebfb170b6d3e25c04bff7
Daniel Marchese <marchese.daniel@gmail.com>
2013-03-15 14:45:32 -0400

    -Added prefs activity and settings ideas

--- gen/com/marcrazysoftware/voicemessenger/R.java
+++ gen/com/marcrazysoftware/voicemessenger/R.java
@@ -17,33 +17,33 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f070000;
-        public static final int bCancel=0x7f07000a;
-        public static final int bMicrophone=0x7f070003;
-        public static final int bReadBack=0x7f070008;
-        public static final int bSend=0x7f070009;
-        public static final int bSendMessage=0x7f070004;
-        public static final int etMessageBody=0x7f070007;
-        public static final int etRecipient=0x7f070006;
-        public static final int llButtonLayout=0x7f070002;
-        public static final int lvMessageView=0x7f070001;
-        public static final int textView=0x7f070005;
+        public static final int LinearLayout1=0x7f080000;
+        public static final int bCancel=0x7f08000a;
+        public static final int bMicrophone=0x7f080003;
+        public static final int bReadBack=0x7f080008;
+        public static final int bSend=0x7f080009;
+        public static final int bSendMessage=0x7f080004;
+        public static final int etMessageBody=0x7f080007;
+        public static final int etRecipient=0x7f080006;
+        public static final int llButtonLayout=0x7f080002;
+        public static final int lvMessageView=0x7f080001;
+        public static final int textView=0x7f080005;
     }
     public static final class layout {
         public static final int main_menu_layout=0x7f030000;
         public static final int send_message_layout=0x7f030001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /** 
@@ -67,10 +67,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+    }
+    public static final class xml {
+        public static final int preferences_layout=0x7f040000;
     }
 }
