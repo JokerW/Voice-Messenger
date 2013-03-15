c61a263290a298799965b943981e3445abbc7e4e
Daniel Marchese <marchese.daniel@gmail.com>
2013-03-14 23:27:01 -0400

    -App now checks for TTS data

--- src/com/marcrazysoftware/voicemessenger/MainMenu.java
+++ src/com/marcrazysoftware/voicemessenger/MainMenu.java
@@ -1,6 +1,8 @@
 package com.marcrazysoftware.voicemessenger;
 
 import java.util.List;
+import java.util.Locale;
+
 import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
@@ -25,11 +27,55 @@ public class MainMenu extends Activity implements OnInitListener,
 
 	private static final int TTS_CODE = 12345;
 
+	private ListView messageListView;
 	private Button micButton;
 	private Button sendMessageButton;
-	private ListView messageListView;
 
 	private TextToSpeech TTS;
+
+	private boolean hasComponents() {
+		/*
+		 * Check for voice recognition.
+		 */
+		PackageManager manager = getPackageManager();
+		List<ResolveInfo> ri = manager.queryIntentActivities(new Intent(
+				RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
+		return ri.size() != 0;
+	}
+
+	private boolean hasSpeechRecognition() {
+		return false;
+	}
+
+	private boolean isConnected() {
+		PackageManager pm = this.getPackageManager();
+		return false;
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == TTS_CODE) {
+			if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+				this.TTS = new TextToSpeech(this, this);
+			} else {
+				Intent installIntent = new Intent();
+				installIntent
+						.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+				startActivity(installIntent);
+			}
+		}
+	}
+
+	@Override
+	public void onBeginningOfSpeech() {
+		this.micButton.setEnabled(false);
+	}
+
+	@Override
+	public void onBufferReceived(byte[] buffer) {
+
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +85,9 @@ public class MainMenu extends Activity implements OnInitListener,
 		/*
 		 * Check for TTS data.
 		 */
+		Intent checkTTSIntent = new Intent();
+		checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+		startActivityForResult(checkTTSIntent, TTS_CODE);
 
 		/*
 		 * Check for voice recognition.
@@ -58,21 +107,6 @@ public class MainMenu extends Activity implements OnInitListener,
 		}
 	}
 
-	private boolean hasComponents() {
-		/*
-		 * Check for voice recognition.
-		 */
-		PackageManager manager = getPackageManager();
-		List<ResolveInfo> ri = manager.queryIntentActivities(new Intent(
-				RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
-		return ri.size() != 0;
-	}
-
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-	}
-
 	@Override
 	protected void onDestroy() {
 		if (this.TTS != null) {
@@ -80,20 +114,6 @@ public class MainMenu extends Activity implements OnInitListener,
 			this.TTS.shutdown();
 		}
 		super.onDestroy();
-	}
-
-	private void runSpeechRecognition() {
-
-	}
-
-	@Override
-	public void onBeginningOfSpeech() {
-		this.micButton.setEnabled(false);
-	}
-
-	@Override
-	public void onBufferReceived(byte[] buffer) {
-
 	}
 
 	@Override
@@ -109,6 +129,20 @@ public class MainMenu extends Activity implements OnInitListener,
 	@Override
 	public void onEvent(int eventType, Bundle params) {
 
+	}
+
+	@Override
+	public void onInit(int status) {
+		/*
+		 * Set the locale to US. Support for other languages will be added
+		 * later.
+		 */
+		if (status == TextToSpeech.SUCCESS) {
+			this.TTS.setLanguage(Locale.US);
+		} else {
+			Toast.makeText(this, "FATAL: TTS HAS FAILED", Toast.LENGTH_LONG)
+					.show();
+		}
 	}
 
 	@Override
@@ -132,12 +166,11 @@ public class MainMenu extends Activity implements OnInitListener,
 
 	}
 
-	@Override
-	public void onInit(int arg0) {
+	private void resultDispatcher(List<String> results) {
 
 	}
 
-	private void resultDispatcher(List<String> results) {
+	private void runSpeechRecognition() {
 
 	}
 
@@ -175,14 +208,5 @@ public class MainMenu extends Activity implements OnInitListener,
 			}
 
 		});
-	}
-
-	private boolean isConnected() {
-		PackageManager pm = this.getPackageManager();
-		return false;
-	}
-
-	private boolean hasSpeechRecognition() {
-		return false;
 	}
 }
