b9c28996f389852bff79aea5cacfe1065dbc8212
Daniel Marchese <marchese.daniel@gmail.com>
2013-03-25 15:42:44 -0400

    -Cleaned up code in MainMenu.java
    Reorganized the functions

--- src/com/marcrazysoftware/voicemessenger/MainMenu.java
+++ src/com/marcrazysoftware/voicemessenger/MainMenu.java
@@ -38,58 +38,6 @@ public class MainMenu extends Activity implements OnInitListener {
 	private TextToSpeech TTS;
 	private SpeechRecognizer recognizer;
 
-	private boolean hasComponents() {
-		/*
-		 * Check for voice recognition.
-		 */
-		PackageManager manager = getPackageManager();
-		List<ResolveInfo> rin = manager.queryIntentActivities(new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
-		return rin.size() != 0;
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		/*
-		 * Inflate the menu.
-		 */
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.menu_layout, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		if (item.getItemId() == R.id.settings) {
-			Intent intent = new Intent(this, PrefsActivity.class);
-			startActivity(intent);
-		}
-
-		return super.onOptionsItemSelected(item);
-	}
-
-	private boolean isConnected() {
-		/*
-		 * Check for a data connection.
-		 */
-		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo netInfo = cm.getActiveNetworkInfo();
-		return netInfo != null && netInfo.isConnected();
-	}
-
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		if (requestCode == TTS_CODE) {
-			if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
-				this.TTS = new TextToSpeech(this, this);
-			} else {
-				Intent installIntent = new Intent();
-				installIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
-				startActivity(installIntent);
-			}
-		}
-	}
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -121,7 +69,74 @@ public class MainMenu extends Activity implements OnInitListener {
 			Toast.makeText(this, "Your device does not support voice recognition =(", Toast.LENGTH_LONG).show();
 		}
 	}
+	
+	private boolean hasComponents() {
+		/*
+		 * Check for voice recognition.
+		 */
+		PackageManager manager = getPackageManager();
+		List<ResolveInfo> rin = manager.queryIntentActivities(new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
+		return rin.size() != 0;
+	}
+	
+	private void setWidgets() {
+		this.messageListView = (ListView) findViewById(R.id.lvMessageView);
+		this.micButton = (Button) findViewById(R.id.bMicrophone);
+		this.sendMessageButton = (Button) findViewById(R.id.bSendMessage);
 
+		this.messageListView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				Toast.makeText(getBaseContext(), "ListView Clicked", Toast.LENGTH_SHORT).show();
+
+			}
+		});
+
+		this.micButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				runSpeechRecognition();
+			}
+
+		});
+
+		this.sendMessageButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				/*
+				 * Start the sendMessageActivity.
+				 */
+				Intent intent = new Intent(MainMenu.this, SendMessageActivity.class);
+				intent.putExtra("hasRecipient", false);
+				startActivity(intent);
+			}
+
+		});
+	}
+	
+	private void runSpeechRecognition() {
+		if (isConnected()) {
+			Intent recogIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+			recogIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, "com.marcrazysoftware.voicemessenger");
+			recogIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+			this.recognizer.startListening(recogIntent);
+		} else {
+			Toast.makeText(this, "Voice Messenger needs a data connection", Toast.LENGTH_LONG).show();
+		}
+	}
+	
+	private boolean isConnected() {
+		/*
+		 * Check for a data connection.
+		 */
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netInfo = cm.getActiveNetworkInfo();
+		return netInfo != null && netInfo.isConnected();
+	}
+	
 	private void setRecognizer() {
 		this.recognizer = SpeechRecognizer.createSpeechRecognizer(this);
 		this.recognizer.setRecognitionListener(new RecognitionListener() {
@@ -177,19 +192,19 @@ public class MainMenu extends Activity implements OnInitListener {
 
 		});
 	}
-
-	@Override
-	protected void onDestroy() {
-		if (this.TTS != null) {
-			this.TTS.stop();
-			this.TTS.shutdown();
+	
+	private void resultDispatcher(List<String> results) {
+		/*
+		 * For now, toast and speek the result for testing purposes.
+		 */
+		if (results.size() > 0) {
+			this.TTS.speak(results.get(0), TextToSpeech.QUEUE_FLUSH, null);
+			Toast.makeText(this, results.get(0), Toast.LENGTH_LONG).show();
+		} else {
+			Toast.makeText(this, "Please Try Again", Toast.LENGTH_LONG).show();
 		}
-		if (this.recognizer != null) {
-			this.recognizer.destroy();
-		}
-		super.onDestroy();
 	}
-
+	
 	@Override
 	public void onInit(int status) {
 		/*
@@ -202,65 +217,50 @@ public class MainMenu extends Activity implements OnInitListener {
 			Toast.makeText(this, "FATAL: TTS HAS FAILED", Toast.LENGTH_LONG).show();
 		}
 	}
-
-	private void resultDispatcher(List<String> results) {
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == TTS_CODE) {
+			if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+				this.TTS = new TextToSpeech(this, this);
+			} else {
+				Intent installIntent = new Intent();
+				installIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+				startActivity(installIntent);
+			}
+		}
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 		/*
-		 * For now, toast and speek the result for testing purposes.
+		 * Inflate the menu.
 		 */
-		if (results.size() > 0) {
-			this.TTS.speak(results.get(0), TextToSpeech.QUEUE_FLUSH, null);
-			Toast.makeText(this, results.get(0), Toast.LENGTH_LONG).show();
-		} else {
-			Toast.makeText(this, "Please Try Again", Toast.LENGTH_LONG).show();
-		}
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu_layout, menu);
+		return true;
 	}
-
-	private void runSpeechRecognition() {
-		if (isConnected()) {
-			Intent recogIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-			recogIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, "com.marcrazysoftware.voicemessenger");
-			recogIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-			this.recognizer.startListening(recogIntent);
-		} else {
-			Toast.makeText(this, "Voice Messenger needs a data connection", Toast.LENGTH_LONG).show();
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.settings) {
+			Intent intent = new Intent(this, PrefsActivity.class);
+			startActivity(intent);
 		}
+
+		return super.onOptionsItemSelected(item);
 	}
-
-	private void setWidgets() {
-		this.messageListView = (ListView) findViewById(R.id.lvMessageView);
-		this.micButton = (Button) findViewById(R.id.bMicrophone);
-		this.sendMessageButton = (Button) findViewById(R.id.bSendMessage);
-
-		this.messageListView.setOnItemClickListener(new OnItemClickListener() {
-
-			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-				Toast.makeText(getBaseContext(), "ListView Clicked", Toast.LENGTH_SHORT).show();
-
-			}
-		});
-
-		this.micButton.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				runSpeechRecognition();
-			}
-
-		});
-
-		this.sendMessageButton.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				/*
-				 * Start the sendMessageActivity.
-				 */
-				Intent intent = new Intent(MainMenu.this, SendMessageActivity.class);
-				intent.putExtra("hasRecipient", false);
-				startActivity(intent);
-			}
-
-		});
+	
+	@Override
+	protected void onDestroy() {
+		if (this.TTS != null) {
+			this.TTS.stop();
+			this.TTS.shutdown();
+		}
+		if (this.recognizer != null) {
+			this.recognizer.destroy();
+		}
+		super.onDestroy();
 	}
 }
