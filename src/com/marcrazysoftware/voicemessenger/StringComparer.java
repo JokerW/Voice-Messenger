cc48849200d1196675efd285c0d48444ba572e02
Daniel Marchese <marchese.daniel@gmail.com>
2013-03-31 20:30:59 -0400

    -Added string comparison framework

--- src/com/marcrazysoftware/voicemessenger/StringComparer.java
+++ src/com/marcrazysoftware/voicemessenger/StringComparer.java
@@ -28,4 +28,50 @@ public abstract class StringComparer {
 		return costs[s2.length()];
 	}
 
+	private static final String getCode(char c) {
+		switch (c) {
+		case 'B':
+		case 'F':
+		case 'P':
+		case 'V':
+			return "1";
+		case 'C':
+		case 'G':
+		case 'J':
+		case 'K':
+		case 'Q':
+		case 'S':
+		case 'X':
+		case 'Z':
+			return "2";
+		case 'D':
+		case 'T':
+			return "3";
+		case 'L':
+			return "4";
+		case 'M':
+		case 'N':
+			return "5";
+		case 'R':
+			return "6";
+		default:
+			return "";
+		}
+	}
+
+	protected static String soundex(String s) {
+		String code, previous, soundex;
+		code = s.toUpperCase().charAt(0) + "";
+		previous = "7";
+		for (int i = 1; i < s.length(); i++) {
+			String current = getCode(s.toUpperCase().charAt(i));
+			if (current.length() > 0 && !current.equals(previous)) {
+				code = code + current;
+			}
+			previous = current;
+		}
+		soundex = (code + "0000").substring(0, 4);
+		return soundex;
+	}
+
 }
